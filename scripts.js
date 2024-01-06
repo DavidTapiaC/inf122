9b1e2f8a4349cca4c803fae1385cf2abe6fb60a7
DavidTapiaC <155449662+DavidTapiaC@users.noreply.github.com>
2024-01-05 21:08:18 -0400

    Js

---
+++ scripts.js
@@ -0,0 +1,61 @@
+// Variables globales
+var nombre = "Juan";
+var apellido = "Sanchez";
+var edad = 30;
+var sueldo = 1.234;
+// Variables de bloque
+let presupuesto = 1000;
+let gastos = 500;
+// Constantes
+const IVA = 13;
+
+// Aritméticos
+let suma = 5 + 5;
+console.log(suma);
+let resta = 5 - 5;
+console.log(resta);
+let multiplicacion = 5 * 5;
+console.log(multiplicacion);
+let division = 5 / 5;
+console.log(division);
+let modulo = 5 % 5;
+console.log(modulo);
+// Comparación
+let mayorQue = 5 > 5;
+console.log(mayorQue);
+let mayorIgualQue = 5 >= 5;
+let menorQue = 5 < 5;
+let menorIgualQue = 5 <= 5;
+let igualQue = 5 === 5;
+let diferenteQue = 5 != 5;
+// Lógicos
+let and = true && true;
+let or = true || false;
+let not = !true;
+
+if (100 % 2 === 0) {
+    console.log('100 es par');
+} else {
+    console.log('100 es no par');
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+let contador = 0;
+while (contador < 5) {
+    contador++;
+    console.log(contador);
+}
+
+const persona = {
+    nombre: "Carlos",
+    edad: 25,
+    ocupacion: "Desarrollador"
+};
+
+const colores = ["rojo", "verde", "azul"];
+
+
+
